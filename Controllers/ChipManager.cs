6835a4a9ec9851fb734ffb697ccaef11ba07350f
Дмитрий Аристов <aristov-dv@mail.ru>
2024-04-14 16:35:01 +0600

    Create ConsoleApp

---
+++ Controllers/ChipManager.cs
@@ -0,0 +1,160 @@
+﻿namespace joseChipsConsole.Controllers
+{
+    public class ChipManager
+    {
+        public static int MinChipMoves(int[] chips, int sumMoves = 0)
+        {
+            int totalChips = CalculateTotalChips(chips);
+            int n = chips.Length;
+            int[] newChips = (int[])chips.Clone();
+
+            if (totalChips % n != 0)
+            {
+                return -1;
+            }
+
+            int target = CalculateTarget(totalChips, n);
+
+            if (newChips.All(chip => chip == target))
+            {
+                return sumMoves;
+            }
+
+            int moves = sumMoves;
+            for (int i = 0; i < n; i++)
+            {
+                if (newChips[i] <= target)
+                {
+                    continue;
+                }
+
+                int diffLeft = CalculateDiffLeft(newChips, i, target);
+                int diffRight = CalculateDiffRight(newChips, i, target);
+                if (diffRight == 0 && diffLeft > 0)
+                {
+                    int l = (i - diffLeft) % n;
+                    int diff = MoveChips(newChips, i, l, target);
+                    moves += CalculateMoves(diff, diffLeft, newChips, i, l);
+                    continue;
+                }
+
+                if (diffLeft == 0 && diffRight > 0)
+                {
+                    int l = (i + diffRight) % n;
+                    int diff = MoveChips(newChips, i, l, target);
+                    moves += CalculateMoves(diff, diffRight, newChips, i, l);
+                    continue;
+                }
+
+                if (diffRight < diffLeft)
+                {
+                    int l = (i + diffRight) % n;
+                    int diff = MoveChips(newChips, i, l, target);
+                    moves += CalculateMoves(diff, diffRight, newChips, i, l);
+                    continue;
+                }
+
+                if (diffLeft < diffRight)
+                {
+                    int l = (i - diffLeft) % n;
+                    int diff = MoveChips(newChips, i, l, target);
+                    moves += CalculateMoves(diff, diffLeft, newChips, i, l);
+                    continue;
+                }
+
+                if (diffRight == diffLeft && diffRight > 0 && diffLeft > 0)
+                {
+                    int l = (i + diffRight) % n;
+                    int diff = MoveChips(newChips, i, l, target);
+                    moves += CalculateMoves(diff, diffRight, newChips, i, l);
+                    continue;
+                }
+            }
+            return MinChipMoves(newChips, moves);
+        }
+
+        private static int CalculateMoves(int diff, int diffValue, int[] chips, int startIndex, int targetIndex)
+        {
+            int minMoves = CalculateMinMoves(chips, startIndex, targetIndex);
+            return diff * Math.Min(diffValue, minMoves);
+        }
+
+        //private static int CalculateMinMoves(int[] chips, int startIndex, int targetIndex)
+        //{
+        //    int n = chips.Length;
+        //    int clockwiseMoves = Math.Abs(targetIndex - startIndex);
+        //    int counterclockwiseMoves = n - clockwiseMoves;
+        //    return Math.Min(clockwiseMoves, counterclockwiseMoves);
+        //}
+
+
+        private static int CalculateMinMoves(int[] chips, int startIndex, int targetIndex)
+        {
+            int n = chips.Length;
+            int clockwiseMoves = Math.Abs(targetIndex - startIndex);
+            int counterclockwiseMoves = n - clockwiseMoves;
+            return Math.Min(clockwiseMoves, counterclockwiseMoves);
+        }
+
+        //private static int CalculateMoves(int diff, int diffValue)
+        //{
+        //    return diff * diffValue;
+        //}
+
+        private static int CalculateTotalChips(int[] chips)
+        {
+            return chips.Sum();
+        }
+
+        private static int CalculateTarget(int totalChips, int n)
+        {
+            return totalChips / n;
+        }
+
+        private static int CalculateDiffLeft(int[] chips, int currentIndex, int targetValue)
+        {
+            int diffLeft = 0;
+            int n = chips.Length;
+            int j = (currentIndex - 1 + n) % n;
+            while (chips[j] < targetValue || (j != currentIndex && chips[j] <= targetValue))
+            {
+                diffLeft += 1;
+                if (chips[j] < targetValue)
+                {
+                    break;
+                }
+                j = (j - 1 + n) % n;
+            }
+            return diffLeft;
+        }
+
+        private static int CalculateDiffRight(int[] chips, int currentIndex, int targetValue)
+        {
+            int diffRight = 0;
+            int n = chips.Length;
+            int k = (currentIndex + 1) % n;
+            while (chips[k] < targetValue || (k != currentIndex && chips[k] <= targetValue))
+            {
+                diffRight += 1;
+                if (chips[k] < targetValue)
+                {
+                    break;
+                }
+                k = (k + 1) % n;
+            }
+            return diffRight;
+        }
+        private static int MoveChips(int[] chips, int currentIndex, int targetIndex, int targetValue)
+        {
+            int n = chips.Length;
+            currentIndex = (currentIndex + n) % n;
+            targetIndex = (targetIndex + n) % n;
+
+            int diff = Math.Min(Math.Abs(chips[currentIndex] - targetValue), Math.Abs(chips[targetIndex] - targetValue));
+            chips[targetIndex] += diff;
+            chips[currentIndex] -= diff;
+            return diff;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using joseChipsConsole.Controllers;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] chips1 = { 8, 10, 2, 5, 5 };
+            int[] chips2 = { 1, 5, 9, 10, 5 };
+            int[] chips3 = { 3, 3, 3, 3, 3, 3 };
+            int[] chips4 = { 0, 0, 0, 0, 0, 0, 0 };
+            int[] chips5 = { 4, 3, 2, 1 };
+            int[] chips6 = { 9, 8, 7, 6 };
+            int[] chips7 = { 5, 5, 5, 5 };
+            int[] chips8 = { 0, 2, 4, 6, 8, 10 };
+            int[] chips9 = { 1, 3, 5, 7, 9, 11 };
+            int[] chips10 = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 };
+
+            int moves1 = ChipManager.MinChipMoves(chips1, 0);
+            int moves2 = ChipManager.MinChipMoves(chips2, 0);
+            int moves3 = ChipManager.MinChipMoves(chips3, 0);
+            int moves4 = ChipManager.MinChipMoves(chips4, 0);
+            int moves5 = ChipManager.MinChipMoves(chips5, 0);
+            int moves6 = ChipManager.MinChipMoves(chips6, 0);
+            int moves7 = ChipManager.MinChipMoves(chips7, 0);
+            int moves8 = ChipManager.MinChipMoves(chips8, 0);
+            int moves9 = ChipManager.MinChipMoves(chips9, 0);
+            int moves10 = ChipManager.MinChipMoves(chips10, 0);
+
+        }
+    }
+}
